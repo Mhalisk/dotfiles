9fa37b4b88a0f2cc0fce397cf4f0bee3ab9020fb
mhalisk <mhalisk.dev@gmail.com>
2025-02-22 18:56:57 -0700

    feat: swapped starship for powerlevel10k

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -1,3 +1,13 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
 # Homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 export HOMEBREW_NO_AUTO_UPDATE=1
@@ -9,13 +19,10 @@ eval "$(pyenv init -)" # Initialize pyenv when a new shell spawns
 
 # OH-MY-ZSH
 export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 plugins=(git gcloud z)
 source $ZSH/oh-my-zsh.sh
 
-# Starship
-export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
-eval "$(starship init zsh)"
-starship config palette $STARSHIP_THEME
 
 # Load Git completion
 zstyle ':completion:*:*:git:*' script $HOME/.config/zsh/git-completion.bash
